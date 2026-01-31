61449dc9c99fb693461de78edaa05f6807124d61
MayconHeat <maycono835@gmail.com>
2026-01-31 13:29:03 -0300

    Add files via upload

---
+++ meu_portifolio/script.js
@@ -0,0 +1,15 @@
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    color: { value: "#ffffff" },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.3
+    },
+    move: { speed: 2 }
+  }
+});
